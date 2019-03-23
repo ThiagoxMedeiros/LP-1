c10b58f9240851acbad277ec2b59accf47e7b2f9
ThiagoxMedeiros <48658033+ThiagoxMedeiros@users.noreply.github.com>
2019-03-22 21:25:36 -0300

    Add files via upload

--- temperaturas.c
+++ temperaturas.c
@@ -2,9 +2,10 @@
 int temps[5];
 int main (void){
 	
-	int temp,soma,i;
+	int temp,soma,i,cont;
 	float media;
 	soma=0;
+	cont=0;
 	for(i=0;i<5;i++){
 		printf("digite uma temperatura menor entre -100 e 100:");
 		scanf("%d",&temp);
@@ -12,9 +13,12 @@ int main (void){
 		soma=soma+temps[i];
 	}
 	media=soma/5;
-	printf("media: %f",media);
-	if (temp>media){
-		printf("%d,temp);}
+	for(i=0;i<5;i++){
+		if(temps[i]>media){
+			cont=cont+1;}
+			}
+	printf("media: %f\n",media);
+	printf("qtd de temperaturas acima da media: %d\n",cont);
 
 	
 	
